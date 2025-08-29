5cd9de5b3dcdc55a0540253a341ad4c950d47c22
Fabrizio <fabrizio.orsini04@gmail.com>
2025-08-29 12:03:06 +0200

    Exite game Action
    Added binding and code for the Exit game action

--- Project-Game/Assets/Scripts/UIManager.cs
+++ Project-Game/Assets/Scripts/UIManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using TMPro;
 
 public class UIManager : MonoBehaviour
@@ -23,7 +24,7 @@ public class UIManager : MonoBehaviour
     private void OnDisable()
     {
         CharacterEvents.characterDamaged -= CharacterTookDamage;
-        CharacterEvents.characterHealed -= CharacterHealed;        
+        CharacterEvents.characterHealed -= CharacterHealed;
     }
 
     public void CharacterTookDamage(GameObject character, int damageReceived)
@@ -44,6 +45,24 @@ public class UIManager : MonoBehaviour
         TMP_Text tmpText = Instantiate(healthTextPrefab, spawnPosition, Quaternion.identity, gameCanvas.transform).GetComponent<TMP_Text>();
 
         tmpText.text = healthRestored.ToString();
-        
+
+    }
+
+    public void OnExitGame(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            #if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+                Debug.Log(this.name + " : " + this.GetType() + " : " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+            #endif
+                        
+            #if (UNITY_EDITOR)
+                UnityEditor.EditorApplication.isPlaying = false;
+            #elif (UNITY_STANDALONE)
+                Application.Quit();
+            #elif (UNITY_WEBGL)
+                SceneManager.LoadScene("QuitScene");
+            #endif
+        }
     }
 }
