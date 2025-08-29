944cf202850f06489ddefea346d256de64db4423
Fabrizio <fabrizio.orsini04@gmail.com>
2025-08-29 16:14:51 +0200

    Health bar UI
    Added implementation for player health bar

--- Project-Game/Assets/Scripts/Damageable.cs
+++ Project-Game/Assets/Scripts/Damageable.cs
@@ -5,6 +5,7 @@ public class Damageable : MonoBehaviour
 
     public UnityEvent<int, Vector2> damageableHit;
     public UnityEvent damageableDeath;
+    public UnityEvent<int, int> healthChanged;
     Animator animator;
 
     [SerializeField]
@@ -33,7 +34,7 @@ public class Damageable : MonoBehaviour
         set
         {
             _health = value;
-
+            healthChanged?.Invoke(_health, MaxHealth);
             // If health drops below 0, character is no longer alive
             if (_health <= 0)
             {
